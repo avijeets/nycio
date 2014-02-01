b73428e486b3344c866f4e5035633aa230623731
carl sednaoui <carlsednaoui@gmail.com>
2014-02-01 13:56:45 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nycio::Application.config.secret_key_base = '85de4eae8353efc82bd537b71a7abdb1c3d2bc2908332fd031641d4c7272cbc422cf634a3fb8d8f273a98cf54994ef546661b2a9a8b8a02481616fb2346185e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nycio::Application.config.session_store :cookie_store, key: '_nycio_session'
